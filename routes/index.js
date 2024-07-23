279b0c94598bf208c0468f42c271a7247bfb6999
Ferdiasnyah Eka Putra <ferdiansyah.chimon24@gmail.com>
2024-07-24 01:47:56 +0700

    fix index route

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.get('Hello World! API berjalan dengan baik...');
+router.get('/', function(req, res) {
+  res.send('Hello World! API berjalan dengan baik...');
 });
 
 module.exports = router;
